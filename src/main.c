dfed583ed092604b655c27165f4a53414fa8cf61
Afrontoso <victor2lra@gmail.com>
2026-09-22 21:51:41 -0300

    adiciona função le16 para leitura de valores de 16 bits e atualiza lógica de verificação de resposta DNS

--- src/main.c
+++ src/main.c
@@ -39,6 +39,10 @@ void escreve16(unsigned char *buf, int pos, unsigned int valor) {
     buf[pos + 1] = valor & 0xFF;
 }
 
+unsigned int le16(unsigned char *buf, int pos) {
+    return (buf[pos] << 8) | buf[pos + 1];
+}
+
 int pula_nome(unsigned char *buf, int pos) {
     while (1) {
         unsigned char tam = buf[pos];
@@ -91,74 +95,91 @@ int main(int argc, char *argv[]) {
     unsigned char buf[512];
     unsigned char resposta[512];
 
-
+    
+    
     if (argc != 3) {
         printf("uso: %s <dominio> <ip_servidor>\n", argv[0]);
         return 1;
     }
-
+    
     srand(time(NULL));
     unsigned int id = rand() & 0xFFFF;
-
+    
     escreve16(buf, 0,  id);
     escreve16(buf, 2,  0x0100);
     escreve16(buf, 4,  0x0001);
     escreve16(buf, 6,  0x0000);
     escreve16(buf, 8,  0x0000);
     escreve16(buf, 10, 0x0000);
-
+    
     int tam_qname = monta_qname(argv[1], buf + 12);
     int pos = 12 + tam_qname;
-
+    
     escreve16(buf, pos, 15);
     pos += 2;
     escreve16(buf, pos, 1);
     pos += 2;
-
+    
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0) {
         printf("erro ao criar socket\n");
         return 1;
     }
-
+    
     struct sockaddr_in servidor;
     memset(&servidor, 0, sizeof(servidor));
     servidor.sin_family = AF_INET;
     servidor.sin_port = htons(53);
-
+    
     if (inet_pton(AF_INET, argv[2], &servidor.sin_addr) != 1) {
         printf("ip invalido: %s\n", argv[2]);
         close(sock);
         return 1;
     }
-
+    
     if (sendto(sock, buf, pos, 0, (struct sockaddr *)&servidor, sizeof(servidor)) < 0) {
         printf("erro ao enviar\n");
         close(sock);
         return 1;
     }
-
+    
     int recebidos = recvfrom(sock, resposta, sizeof(resposta), 0, NULL, NULL);
     if (recebidos < 0) {
         printf("erro ao receber\n");
         close(sock);
         return 1;
     }
-
-    printf("resposta com %d bytes:\n", recebidos);
-    for (int i = 0; i < recebidos; i++) {
-        printf("%02X ", resposta[i]);
-        if ((i + 1) % 12 == 0) printf("\n");
+    
+    unsigned int rcode = resposta[3] & 0x0F;
+    unsigned int ancount = le16(resposta, 6);
+    
+    if (rcode == 3) {
+        printf("Dominio %s nao encontrado\n", argv[1]);
+        close(sock);
+        return 0;
     }
-    printf("\n");
+
+    if (rcode != 0 || ancount == 0) {
+        printf("Dominio %s nao possui entrada MX\n", argv[1]);
+        close(sock);
+        return 0;
+    }
+
 
     int p = 12;
     p = pula_nome(resposta, p);
     p += 4;
 
-    printf("resposta comeca no byte %d\n", p);
-
     p = pula_nome(resposta, p);
+
+    unsigned int tipo = le16(resposta, p);
+
+    if (tipo != 15) {
+        printf("Dominio %s nao possui entrada MX\n", argv[1]);
+        close(sock);
+        return 0;
+    }
+
     p += 12;
 
     char nome_mx[256];
